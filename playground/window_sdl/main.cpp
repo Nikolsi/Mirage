4ca5f65600951a7e4ca8708802093c6bd80590c7
Nikolai Arsenov <nikolai.a.arsenov@hotmail.com>
2021-01-10 00:41:22 +0100

    window handling sdl example

---
+++ playground/window_sdl/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <memory>
+
+#include <mirage.h>       // core
+#include <mirage_sdl.hpp> // sdl specific
+
+// #include <SDL.h>
+
+std::shared_ptr<SDL_Window> createApplicationSDL();
+
+void destroyApplicationSDL(std::shared_ptr<SDL_Window> window);
+
+int main(int argc, char **argv)
+{
+    // create window
+    auto window = createApplicationSDL();
+
+    // create rendering context
+    auto context = Mirage::SDL::Context(window);
+
+    // create rendering engine
+    auto engine = Mirage::Engine(context);
+
+    // event loop
+    bool done = false;
+    while (!done)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                done = true;
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+    }
+
+    // destroy rendering engine
+    engine.dispose();
+
+    // destroy window
+    destroyApplicationSDL(window);
+
+    return 0;
+}
+
+std::shared_ptr<SDL_Window> createApplicationSDL()
+{
+    // setup SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // setup window
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window *window = SDL_CreateWindow("Mirage + SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+
+    return std::make_shared<SDL_Window>(window)
+}
+
+void destroyApplicationSDL(std::shared_ptr<SDL_Window> window)
+{
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
