605059d643da172bd0774951701cf7a7a5767bd4
Nikolai Arsenov <Nikolsi@users.noreply.github.com>
2022-02-20 01:16:19 +0100

    Support objective-c-cxx apps (#10)

--- playground/metal_sdl/main.mm
+++ playground/metal_sdl/main.mm
@@ -1,56 +1,60 @@
+// Original: https://gist.github.com/gcatlin/b89e0efed78dd91364609ca4095da346
+// w/o CMake: cc main.mm `sdl2-config --cflags --libs`
+// -framework Metal -framework QuartzCore && ./a.out
 //
-// TODO: add CMake build support ObjectiveC/C++
-// see: https://stackoverflow.com/questions/20962869/cmakelists-txt-for-an-objective-c-project
-// original: https://gist.github.com/gcatlin/b89e0efed78dd91364609ca4095da346
-// cc main.mm `sdl2-config --cflags --libs` -framework Metal -framework QuartzCore && ./a.out
-//
-#include <SDL.h>
 #import <Metal/Metal.h>
 #import <QuartzCore/CAMetalLayer.h>
+#include <SDL.h>
 
-int main (int argc, char *args[])
-{
-    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "metal");
-    SDL_InitSubSystem(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("SDL Metal", -1, -1, 640, 480, SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-    const CAMetalLayer *swapchain = (__bridge CAMetalLayer *)SDL_RenderGetMetalLayer(renderer);
-    const id<MTLDevice> gpu = swapchain.device;
-    const id<MTLCommandQueue> queue = [gpu newCommandQueue];
+int main(int argc, char *args[]) {
+  SDL_SetHint(SDL_HINT_RENDER_DRIVER, "metal");
+  SDL_InitSubSystem(SDL_INIT_VIDEO);
+  SDL_Window *window =
+      SDL_CreateWindow("SDL Metal", -1, -1, 640, 480, SDL_WINDOW_ALLOW_HIGHDPI);
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+  const CAMetalLayer *swapchain =
+      (__bridge CAMetalLayer *)SDL_RenderGetMetalLayer(renderer);
+  const id<MTLDevice> gpu = swapchain.device;
+  const id<MTLCommandQueue> queue = [gpu newCommandQueue];
 
-    MTLClearColor color = MTLClearColorMake(0, 0, 0, 1);
-    bool quit = false;
-    SDL_Event e;
+  MTLClearColor color = MTLClearColorMake(0, 0, 0, 1);
+  bool quit = false;
+  SDL_Event e;
 
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            switch (e.type) {
-                case SDL_QUIT: quit = true; break;
-            }
-        }
-
-        @autoreleasepool {
-            id<CAMetalDrawable> surface = [swapchain nextDrawable];
-
-            color.red = (color.red > 1.0) ? 0 : color.red + 0.01;
-
-            MTLRenderPassDescriptor *pass = [MTLRenderPassDescriptor renderPassDescriptor];
-            pass.colorAttachments[0].clearColor = color;
-            pass.colorAttachments[0].loadAction  = MTLLoadActionClear;
-            pass.colorAttachments[0].storeAction = MTLStoreActionStore;
-            pass.colorAttachments[0].texture = surface.texture;
-
-            id<MTLCommandBuffer> buffer = [queue commandBuffer];
-            id<MTLRenderCommandEncoder> encoder = [buffer renderCommandEncoderWithDescriptor:pass];
-            [encoder endEncoding];
-            [buffer presentDrawable:surface];
-            [buffer commit];
-        }
+  while (!quit) {
+    while (SDL_PollEvent(&e) != 0) {
+      switch (e.type) {
+      case SDL_QUIT:
+        quit = true;
+        break;
+      }
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    @autoreleasepool {
+      id<CAMetalDrawable> surface = [swapchain nextDrawable];
 
-    return 0;
+      color.red = (color.red > 1.0) ? 0 : color.red + 0.01;
+
+      MTLRenderPassDescriptor *pass =
+          [MTLRenderPassDescriptor renderPassDescriptor];
+      pass.colorAttachments[0].clearColor = color;
+      pass.colorAttachments[0].loadAction = MTLLoadActionClear;
+      pass.colorAttachments[0].storeAction = MTLStoreActionStore;
+      pass.colorAttachments[0].texture = surface.texture;
+
+      id<MTLCommandBuffer> buffer = [queue commandBuffer];
+      id<MTLRenderCommandEncoder> encoder =
+          [buffer renderCommandEncoderWithDescriptor:pass];
+      [encoder endEncoding];
+      [buffer presentDrawable:surface];
+      [buffer commit];
+    }
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
